60e37bd6fc7cd7983892a975ed9e9acad4e1015e
hasnanaziya <hasnanaziya26@gmail.com>
2024-08-10 06:44:29 -0400

    Adding model changes tp Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,19 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['car_make', 'name', 'type']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
