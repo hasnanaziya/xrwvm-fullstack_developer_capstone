2de053d3d577886ffefb21872be00fd5ede4304a
hasnanaziya <144528367+hasnanaziya@users.noreply.github.com>
2024-08-13 01:20:53 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -67,8 +67,11 @@ def registration(request):
         logger.debug("{} is a new user".format(username))
 
     if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name,
-                                        last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(username=username, 
+                                        first_name=first_name,
+                                        last_name=last_name, 
+                                        password=password, 
+                                        email=email)
 
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
